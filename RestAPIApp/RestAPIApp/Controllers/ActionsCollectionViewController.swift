0be5497225012bf54fe42fe0ee19b70480e5fc2f
UlianaPereverzeva <115875780+UlianaPereverzeva@users.noreply.github.com>
2022-12-27 17:26:08 +0300

    created screen with 2 cells

---
+++ RestAPIApp/RestAPIApp/Controllers/ActionsCollectionViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ActionsCollectionViewController.swift
+//  RestAPIApp
+//
+//  Created by ульяна on 27.12.22.
+//
+
+import UIKit
+
+class ActionsCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    private var collectionView: UICollectionView?
+    
+    private let reuseIdentifier = "Cell"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureCollectionView()
+        self.view.backgroundColor = .systemPink
+    }
+    
+    private func configureCollectionView() {
+        let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        
+        self.collectionView = collectionView
+        self.view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    // MARK: UICollectionViewDataSource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? MyCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(title: "Hello!", backgroundColor: .white)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width - 20, height: 70)
+    }
+    
+    // MARK: UICollectionViewDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    /*
+     // Uncomment this method to specify if the specified item should be highlighted during tracking
+     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment this method to specify if the specified item should be selected
+     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+     
+     }
+     */
+    
+}
---
+++ RestAPIApp/RestAPIApp/Controllers/MyCollectionViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  MyCollectionViewCell.swift
+//  RestAPIApp
+//
+//  Created by ульяна on 27.12.22.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    private var titleLabel: UILabel?
+    
+    func configure(title: String, backgroundColor: UIColor) {
+        if titleLabel == nil {
+            self.addTitleLabel()
+        }
+        
+        titleLabel?.font = UIFont.systemFont(ofSize: 22, weight: .black)
+        titleLabel?.textColor = .systemPink
+        titleLabel?.text = title
+        self.contentView.backgroundColor = backgroundColor
+    }
+    
+    private func addTitleLabel() {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.titleLabel = label
+        
+        self.contentView.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor)
+        ])
+    }
+    
+    private func configureUI() {
+        
+    }
+}
